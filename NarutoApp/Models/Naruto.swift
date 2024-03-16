8f9de0eff06a8a99e737a53237e6b98db0e30dde
Nikita <fen1kser@yandex.ru>
2024-03-16 14:49:03 +0300

    Naruto-3: Сделать экран детально о персонаже 1. Сделал экран детально о персонаже 2. Поработал с элементами интерфейса экрана 3. Проработал лучше модель

--- NarutoApp/Models/Naruto.swift
+++ NarutoApp/Models/Naruto.swift
@@ -13,35 +13,93 @@ struct Naruto: Decodable {
 
 struct Character: Decodable {
     let name: String
-    let images: [URL?]?
-//    let family: [String: String]?
-    let jutsu: [String?]?
-//    let natureType: [String?]?
-//    let personal: [Personal?]?
-//    let uniqueTraits: [String?]
+    let images: [URL]?
+    let family: Family?
+//    let personal: Personal?
+    let jutsu: [String]?
+    let natureType: [String]?
+    
+    var description: String {
+        """
+        Name: \(name)
+        """
+    }
     
 }
 
 struct Personal: Decodable {
+    let species: String?
+    let status: Status?
+    let kekkeiGenkai, classification: String?
+    let jinchūriki, titles: [String]?
     let birthdate: String?
-    let sex: String?
-    let age: Parameters?
-    let status: String?
-    let height: Parameters?
-    let weight: Parameters?
+    let sex: Sex?
+    let height: Height?
+    let weight: Weight?
     let bloodType: String?
-    let occupation: String?
-    let team: String?
     let partner: String?
+    let age: Age?
+    let clan: String?
 }
 
-struct Parameters: Decodable {
-    let partOne: String?
-    let partTwo: String?
-    
+enum Status: String, Decodable {
+    case deceased = "Deceased"
+    case incapacitated = "Incapacitated"
+}
+
+enum Sex: String, Decodable {
+    case female = "Female"
+    case male = "Male"
+}
+
+struct Weight: Codable {
+    let partTwo, partOne: String?
+
     enum CodingKeys: String, CodingKey {
-        case partOne = "Part I"
         case partTwo = "Part II"
+        case partOne = "Part I"
+    }
+}
+
+struct Height: Decodable {
+    let partII, partI, blankPeriod, gaiden: String?
+
+    enum CodingKeys: String, CodingKey {
+        case partII = "Part II"
+        case partI = "Part I"
+        case blankPeriod = "Blank Period"
+        case gaiden = "Gaiden"
+    }
+}
+
+struct Age: Decodable {
+    let partTwo, partOne, academyGraduate, chuninPromotion: String?
+    let borutoManga: String?
+
+    enum CodingKeys: String, CodingKey {
+        case partTwo = "Part II"
+        case partOne = "Part I"
+        case academyGraduate = "Academy Graduate"
+        case chuninPromotion = "Chunin Promotion"
+        case borutoManga = "Boruto Manga"
+    }
+}
+
+struct Family: Decodable {
+    let incarnationWithTheGodTree, depoweredForm, son, nephew: String?
+    let adoptiveSon, father, adoptiveBrother, cousin: String?
+    let adoptiveFather, brother, husband: String?
+
+    enum CodingKeys: String, CodingKey {
+        case incarnationWithTheGodTree = "incarnation with the god tree"
+        case depoweredForm = "depowered form"
+        case son, nephew
+        case adoptiveSon = "adoptive son"
+        case father
+        case adoptiveBrother = "adoptive brother"
+        case cousin
+        case adoptiveFather = "adoptive father"
+        case brother, husband
     }
 }
 
---
+++ NarutoApp/ViewControllers/DetailsCharacterViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailsCharacterViewController.swift
+//  NarutoApp
+//
+//  Created by Никита on 16.03.2024.
+//
+
+import UIKit
+
+final class DetailsCharacterViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var characterImageView: UIImageView! {
+        didSet {
+            characterImageView.contentMode = .scaleAspectFit
+            characterImageView.clipsToBounds = true
+            characterImageView.layer.cornerRadius = characterImageView.frame.height / 2
+            characterImageView.backgroundColor = .white
+        }
+    }
+    @IBOutlet weak var characterDescriprionLabel: UILabel!
+    
+    // MARK: - Public properties
+    var character: Character!
+    
+    // MARK: - Private Properties
+    private let networkManager = NetworkManager.shared
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        characterDescriprionLabel.text = character.description
+        fetchImage()
+    }
+    
+    private func fetchImage() {
+        guard let imageURL = character.images?.first else { return }
+        networkManager.fetchImage(from: imageURL) { result in
+            switch result {
+            case .success(let image):
+                self.characterImageView.image = UIImage(data: image)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
